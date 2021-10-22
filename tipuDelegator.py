9f796ca311007fd108b7c43db5698207c315e8bd
Owenn <owenn.com@gmail.com>
2021-10-22 13:25:22 -0500

    added gitignore

--- tipuDelegator.py
+++ tipuDelegator.py
@@ -20,8 +20,22 @@ while True:
 	account = Account(user, blockchain_instance=steem)
 	effectiveSteemPower = steem.vests_to_token_power(account.get_effective_vesting_shares() / (10 ** 6))
 	if(effectiveSteemPower  > 0.1):
-		account.delegate_vesting_shares(tipu, str(float(account.balances['total'][2]) - 1))
-		print(user, 'delegated to tipu')
+		currentDelegations = account.get_vesting_delegations()
+		otherDelegationShares = 0.0
+		if currentDelegations:
+			for i in currentDelegations:
+				if(i['delegatee'] != 'tipu'):
+					print(i)
+					amount = i['vesting_shares']['amount']
+					otherDelegationShares = float(amount) + otherDelegationShares
+			otherDelegationShares = otherDelegationShares / (10 ** 6)
+		bal = account.balances['total'][2]
+		bal = str(bal).split()
+		ballance = bal[0]
+		amountToDelegate = float(float(ballance) - float(otherDelegationShares)) - 1
+		print(amountToDelegate)
+		account.delegate_vesting_shares(tipu, str(amountToDelegate) + ' VESTS')
+		print(user, 'delegated', amountToDelegate, 'shares to tipu')
 	else:
 		print(user, 'didnt delegate to tipu')
 	print('zzz...')
